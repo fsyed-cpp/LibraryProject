7491fbb13b457172827f92060b7a0957ffdb0106
Faisal-Syed <fas2001@g4vxvhn7qj.lan>
2023-08-05 16:46:42 -0700

    Added default loans landing page, added error dialogue popup when search button is pressed

--- src/main/java/com/example/libraryproject/LibraryApplication.java
+++ src/main/java/com/example/libraryproject/LibraryApplication.java
@@ -1,23 +1,110 @@
 package com.example.libraryproject;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class LibraryApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+
+        // Library Management Title
+        Label titleLabel = new Label("Library Management");
+        titleLabel.setStyle("-fx-font-size: 24px;");
+        HBox titleBar = new HBox(titleLabel);
+        titleBar.setPadding(new Insets(10));
+        titleBar.setAlignment(Pos.CENTER);
+        titleBar.setStyle("-fx-background-color: #e0e0e0;");
+
+        // 3 Button Menu
+        HBox buttonBox = getMenuBox();
+
+        // Combine title and buttons into top portion
+        VBox titleBox = new VBox(10, titleBar, buttonBox);
+
+        // Tabs for Standard and Comprehensive
+        TabPane tabPane = new TabPane();
+        Tab standardTab = new Tab("Standard");
+        Tab comprehensiveTab = new Tab("Comprehensive");
+        tabPane.getTabs().addAll(standardTab, comprehensiveTab);
+
+        // Search bar and button
+        TextField searchBar = new TextField();
+        searchBar.setPromptText("Search...");
+        Button searchButton = new Button("Search");
+
+        // Search button functionality:
+        searchButton.setOnAction(event -> {
+            String searchText = searchBar.getText();
+            boolean found = false;
+            String line;
+
+            try (BufferedReader br = new BufferedReader(new FileReader("path/to/loans.csv"))) {
+                while ((line = br.readLine()) != null) {
+                    // Split the line by comma and check if it contains the search text
+                    String[] values = line.split(",");
+                    for (String value : values) {
+                        if (value.equals(searchText)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found) break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (!found) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Alert");
+                alert.setHeaderText("Bronco ID not found in database");
+                alert.setContentText("Please check to make sure the ID is correct");
+                alert.showAndWait();
+            }
+        });
+
+        HBox searchBox = new HBox(10, searchBar, searchButton); // 10 is the spacing between elements
+        searchBox.setAlignment(Pos.CENTER);
+
+        // Main layout
+        VBox mainLayout = new VBox(10, titleBox, buttonBox, tabPane, searchBox); // 10 is the spacing between elements
+
+        // Scene
+        Scene scene = new Scene(mainLayout, 800, 600);
+        primaryStage.setTitle("Library Management");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private static HBox getMenuBox() {
+        ToggleButton loansCTA = new ToggleButton("Loans");
+        ToggleButton peopleCTA = new ToggleButton("People");
+        ToggleButton inventoryCTA = new ToggleButton("Inventory");
+
+        ToggleGroup menuGroup = new ToggleGroup();
+        loansCTA.setToggleGroup(menuGroup);
+        peopleCTA.setToggleGroup(menuGroup);
+        inventoryCTA.setToggleGroup(menuGroup);
+
+        // By default, set loans to be selected when the app runs
+        loansCTA.setSelected(true);
+
+        HBox buttonBox = new HBox(10, loansCTA, peopleCTA, inventoryCTA); // 10 is the spacing between buttons
+        buttonBox.setAlignment(Pos.CENTER);
+        return buttonBox;
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/com/example/libraryproject/LibraryController.java
+++ src/main/java/com/example/libraryproject/LibraryController.java
@@ -3,7 +3,7 @@ package com.example.libraryproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LibraryController {
     @FXML
     private Label welcomeText;
 
