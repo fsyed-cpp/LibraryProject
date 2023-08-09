ea6751498ae2530066093dc781f6de771b002234
SHyguymoll <shymollet@gmail.com>
2023-08-08 23:39:05 -0700

    switched over to a linked hash map

--- src/main/java/com/example/libraryproject/CSVController.java
+++ src/main/java/com/example/libraryproject/CSVController.java
@@ -1,101 +1,102 @@
 package com.example.libraryproject;
 import java.io.*;
+import java.util.LinkedHashMap;
 import java.util.ArrayList;
 public class CSVController {
     private String fileString;
-    private ArrayList<String[]> csvData;
-    public CSVController(String fileStr) {
+    private ArrayList<LinkedHashMap<String, String>> csvData;
+    public CSVController(String fileStr, String[] keyNames) {
         fileString = fileStr;
-        csvData = new ArrayList<String[]>();
+        csvData = new ArrayList<>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileString));
-            String line;
-            while ((line = br.readLine()) != null)
-                csvData.add(line.split(","));
+            String line = br.readLine();
+            int which;
+            while ((line = br.readLine()) != null) {
+                which = 0;
+                LinkedHashMap<String, String> entry = null;
+                for (String sepLine : line.split(",")) {
+                    entry = new LinkedHashMap<>();
+                    entry.put(keyNames[which], sepLine);
+                    which++;
+                }
+                csvData.add(entry);
+            }
             br.close();
         } catch (IOException e) {
-            csvData = new ArrayList<String[]>();
+            csvData = new ArrayList<LinkedHashMap<String, String>>();
         }
     }
-    public CSVController() {
-        super();
-    }
-    public ArrayList<String[]> getCsvData() {
+    public ArrayList<LinkedHashMap<String, String>> getCsvData() {
         return csvData;
     }
-    public void setCsvData(ArrayList<String[]> csvData) {
+    public void setCsvData(ArrayList<LinkedHashMap<String, String>> csvData) {
         this.csvData = csvData;
     }
 
     public void UpdateCSV() {
         try {
             FileWriter fw = new FileWriter(fileString);
-            for (String[] lines : csvData.toArray(new String[0][0]))
-                for (String line : lines)
-                    fw.write(line);
+            for (int i = 0; i < this.csvData.size(); i++) {
+                LinkedHashMap<String, String> curMap = this.csvData.get(i);
+                for (String line : curMap.values()) {
+                    fw.write(line + ",");
+                }
                 fw.write("\n");
+            }
             fw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public boolean search(String searchQuery) {
-        for (String[] lines : this.getCsvData())
-            for (String line : lines)
-                if (line.equals(searchQuery)) {
-                    return true;
-                }
-        return false;
-    }
 
-    public boolean search(String searchQuery, int index) {
-        for (String[] lines : this.getCsvData())
-            if (lines[index].equals(searchQuery)) {
+    public boolean search(String searchQuery, String keyName) {
+        for (LinkedHashMap<String, String> lines : this.getCsvData()) {
+            if (lines.get(keyName).equals(searchQuery)) {
                 return true;
             }
+        }
         return false;
     }
 
-    public String searchAndReturnString(String searchQuery) {
-        int ind = 0;
-        for (String[] lines : this.getCsvData()) {
-            for (String line : lines)
-                if (line.equals(searchQuery)) {
-                    return String.join(",", this.getCsvData().get(ind));
-                }
-            ind++;
-        }
-        return null;
+
+    public String[] returnArraySlice(String... keyNames) {
+        ArrayList<String> toRet = new ArrayList<>();
+        for (LinkedHashMap<String, String> lines : this.getCsvData())
+            for (String keyName : keyNames)
+                toRet.add(lines.get(keyName));
+        return toRet.toArray(new String[0]);
     }
 
-    public String searchAndReturnString(String searchQuery, int index) {
+
+    public String searchAndReturnString(String searchQuery, String keyName) {
         int ind = 0;
-        for (String[] lines : this.getCsvData()) {
-            if (lines[index].equals(searchQuery)) {
-                return String.join(",", this.getCsvData().get(ind));
+        for (LinkedHashMap<String, String> lines : this.getCsvData()) {
+            if (lines.get(keyName).equals(searchQuery)) {
+                return String.join(",", lines.values());
             }
             ind++;
         }
         return null;
     }
 
-    public String[] searchAndReturnStringArray(String searchQuery) {
+    public ArrayList<LinkedHashMap<String, String>> searchAndReturnSetOfHashMaps(String searchQuery, String keyName) {
+        ArrayList<LinkedHashMap<String, String>> validArrays = new ArrayList<>();
         int ind = 0;
-        for (String[] lines : this.getCsvData()) {
-            for (String line : lines)
-                if (line.equals(searchQuery)) {
-                    return this.getCsvData().get(ind);
-                }
+        for (LinkedHashMap<String, String> lines : this.getCsvData()) {
+            if (lines.get(keyName).equals(searchQuery)) {
+                validArrays.add(this.getCsvData().get(ind));
+            }
             ind++;
         }
-        return null;
+        return validArrays;
     }
 
-    public String[] searchAndReturnStringArray(String searchQuery, int index) {
+    public LinkedHashMap<String, String> searchAndReturnHashMap(String searchQuery, String keyName) {
         int ind = 0;
-        for (String[] lines : this.getCsvData()) {
-            if (lines[index].equals(searchQuery)) {
+        for (LinkedHashMap<String, String> lines : this.getCsvData()) {
+            if (lines.get(keyName).equals(searchQuery)) {
                 return this.getCsvData().get(ind);
             }
             ind++;
@@ -107,7 +108,7 @@ public class CSVController {
         this.getCsvData().remove(ind);
     }
 
-    public void addLine(String[] content) {
+    public void addLine(LinkedHashMap<String, String> content) {
         this.getCsvData().add(content);
     }
 }
